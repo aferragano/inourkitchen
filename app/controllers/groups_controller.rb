ad258fbb28c3232a058eead43ab75a382c7101ee
Anthony Ferragano <aferragano@gmail.com>
2014-12-01 16:00:54 -0500

    group create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,43 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+	def index
+		@group = Group.new
+		@groups = Group.all
+	end
+
+	def new
+		# @group = Group.new
+	end
+
+	def create
+		@group = Group.create(group_params)
+		if @group.save
+			redirect_to @group
+		else
+			render :new
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+	
+	private
+
+	def set_group
+		@group = Group.find(params[:id])
+	end
+
+	def group_params
+		params.require(:group).permit(:name)
+	end
 
 end
