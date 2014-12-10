077087af3599fd3733935ad2f34cf2a37db9da25
Anthony Ferragano <aferragano@gmail.com>
2014-12-10 11:46:19 -0500

    delete space in migration

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,8 @@
 class Story < ActiveRecord::Base
 	belongs_to :user
 	
-	has_many :recipe_stories 
+	has_many :recipe_stories
+	
 	# has_many :recipes, through: recipe_stories
 
 	has_many :comments, :as => :commentable 
