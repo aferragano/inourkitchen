43a9b3bf8f3635218e88174bc370d60ca40af71b
Anthony Ferragano <aferragano@gmail.com>
2014-12-15 13:47:28 -0500

    add validations to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,12 @@ class User < ActiveRecord::Base
 	has_many :groups, through: :group_users
 
 	has_secure_password
+
 	validates :email, presence: true, 
 						uniqueness: true,
             format: { :with => /\w+@\w+\.\w+/}
 	validates :username, presence: true,
-             uniqueness: true
+            uniqueness: true
+	validates :password,   presence: true,
+           	length: { in: 6..20 }
 end
