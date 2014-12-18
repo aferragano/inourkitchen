a8223057f11e5f903f040b4db74378e62cf91dd1
Anthony Ferragano <aferragano@gmail.com>
2014-12-18 17:13:56 -0500

    add rand picture sample and also tried to add images to .gitignore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,14 @@ module ApplicationHelper
 		end
 	end 
 
+	def rand_image(images)
+		lib = []
+		images.each do |img|
+			lib << img.image_url
+		end
+		lib.sample
+	end 
+
 	def created_at(item)
         item.created_at.strftime("%m/%d/%y")
    end
