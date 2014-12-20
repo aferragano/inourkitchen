ba58006641b3311cb80a42c90c6abf2591b7803e
Anthony Ferragano <aferragano@gmail.com>
2014-12-19 22:39:46 -0500

    replace first_image helper with last image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-	def first_image(images)
+	def last_image(images)
 		lib = []
 		images.each do |img|
 			lib << img.image_url
 		end
-		if lib[0]
-			lib[0]
+		if lib[-1]
+			lib[-1]
 		end
 	end 
 
