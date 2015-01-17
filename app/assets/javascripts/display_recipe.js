b421e0f3f602c78864b1fe085569e1777b760a3c
Anthony Ferragano <aferragano@gmail.com>
2015-01-17 14:15:49 -0500

    about and careers tab in footer

--- app/assets/javascripts/display_recipe.js
+++ app/assets/javascripts/display_recipe.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
 	hideShow($('#title'),$('#summary'));
 	hideShow($('#comment-recipe-btn'),$('.comment'));
 	hideShow($('#stories-recipe-btn'),$(''));
+	hideShow($('#hints-btn'),$('#hints'));
+	hideShow($('#about-btn'),$('#about'));
+	hideShow($('#careers-btn'),$('#careers'));
+
 });
