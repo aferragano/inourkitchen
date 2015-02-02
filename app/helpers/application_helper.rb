6a6b95214e5f8227f42a68b3c3c543bad4b3479e
Anthony Ferragano <aferragano@gmail.com>
2015-02-02 15:33:36 -0500

    the things added for sass

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,3 +22,20 @@ module ApplicationHelper
         item.created_at.strftime("%m/%d/%y")
    end
 end
+
+module Sass
+  module Tree
+    class ImportNode < RootNode
+      def _dump(f)
+        Marshal.dump([@imported_filename, children])
+      end
+
+      def self._load(data)
+        filename, children = Marshal.load(data)
+        node = ImportNode.new(filename)
+        node.children = children
+        node
+      end
+    end
+  end
+end
